71655df2f720e6d26e930f479c4f44ac54507d97
Matthew James Hodgson <matthodg@uvic.ca>
2018-02-23 16:19:29 -0800

    Prototype of how Github Radar will look
    Added some hard coded data to visualize what Github radar will probably
    look like.

--- Script.js
+++ Script.js
@@ -4,56 +4,31 @@ var w = 500,
 var colorscale = d3.scale.category10();
 
 //Legend titles
-var LegendOptions = ['Smartphone', 'Tablet'];
+var LegendOptions = ['Fred', 'Bob', 'Joe'];
 
 //Data
 var d = [
     [
-        { axis: "Email", value: 0.59 },
-        { axis: "Social Networks", value: 0.56 },
-        { axis: "Internet Banking", value: 0.42 },
-        { axis: "News Sportsites", value: 0.34 },
-        { axis: "Search Engine", value: 0.48 },
-        { axis: "View Shopping sites", value: 0.14 },
-        { axis: "Paying Online", value: 0.11 },
-        { axis: "Buy Online", value: 0.05 },
-        { axis: "Stream Music", value: 0.07 },
-        { axis: "Online Gaming", value: 0.12 },
-        { axis: "Navigation", value: 0.27 },
-        { axis: "App connected to TV program", value: 0.03 },
-        { axis: "Offline Gaming", value: 0.12 },
-        { axis: "Photo Video", value: 0.4 },
-        { axis: "Reading", value: 0.03 },
-        { axis: "Listen Music", value: 0.22 },
-        { axis: "Watch TV", value: 0.03 },
-        { axis: "TV Movies Streaming", value: 0.03 },
-        { axis: "Listen Radio", value: 0.07 },
-        { axis: "Sending Money", value: 0.18 },
-        { axis: "Other", value: 0.07 },
-        { axis: "Use less Once week", value: 0.08 }
+        { axis: "Commits", value: 0.10 },
+        { axis: "Added LoC", value: 0.10 },
+        { axis: "Removed Loc", value: 0.30 },
+        { axis: "Branches", value: 0.70 },
+        { axis: "Merges", value: 0.50 },
+        { axis: "Pull Requests", value: 0.70 }
     ], [
-        { axis: "Email", value: 0.48 },
-        { axis: "Social Networks", value: 0.41 },
-        { axis: "Internet Banking", value: 0.27 },
-        { axis: "News Sportsites", value: 0.28 },
-        { axis: "Search Engine", value: 0.46 },
-        { axis: "View Shopping sites", value: 0.29 },
-        { axis: "Paying Online", value: 0.11 },
-        { axis: "Buy Online", value: 0.14 },
-        { axis: "Stream Music", value: 0.05 },
-        { axis: "Online Gaming", value: 0.19 },
-        { axis: "Navigation", value: 0.14 },
-        { axis: "App connected to TV program", value: 0.06 },
-        { axis: "Offline Gaming", value: 0.24 },
-        { axis: "Photo Video", value: 0.17 },
-        { axis: "Reading", value: 0.15 },
-        { axis: "Listen Music", value: 0.12 },
-        { axis: "Watch TV", value: 0.1 },
-        { axis: "TV Movies Streaming", value: 0.14 },
-        { axis: "Listen Radio", value: 0.06 },
-        { axis: "Sending Money", value: 0.16 },
-        { axis: "Other", value: 0.07 },
-        { axis: "Use less Once week", value: 0.17 }
+        { axis: "Commits", value: 0.30 },
+        { axis: "Added LoC", value: 0.20 },
+        { axis: "Removed Loc", value: 0 },
+        { axis: "Branches", value: 0.20 },
+        { axis: "Merges", value: 0.50 },
+        { axis: "Pull Requests", value: 0.10 }
+    ], [
+        { axis: "Commits", value: 0.60 },
+        { axis: "Added LoC", value: 0.70 },
+        { axis: "Removed Loc", value: 0.60 },
+        { axis: "Branches", value: 0.10 },
+        { axis: "Merges", value: 0 },
+        { axis: "Pull Requests", value: 0.20 }
     ]
 ];
 
@@ -61,8 +36,8 @@ var d = [
 var mycfg = {
     w: w,
     h: h,
-    maxValue: 0.6,
-    levels: 6,
+    maxValue: 1,
+    levels: 5,
     ExtraWidthX: 300
 }
 
@@ -88,7 +63,7 @@ var text = svg.append("text")
     .attr("y", 10)
     .attr("font-size", "12px")
     .attr("fill", "#404040")
-    .text("What % of owners use a specific service in a week");
+    .text("What percent of the total each user has contributed");
 
 //Initiate Legend	
 var legend = svg.append("g")
